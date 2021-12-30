23615dc384338617db748559876500ac2ec401e4
oralridvan <oral.ridvan@hotmail.com>
2021-12-30 12:36:37 -0500

    asdf

--- src/test/java/Udemy/SpecBuilderTest.java
+++ src/test/java/Udemy/SpecBuilderTest.java
@@ -19,30 +19,30 @@ import static io.restassured.RestAssured.given;
 public class SpecBuilderTest {
     public static void main(String[] args) {
 
-        addPlace p=new addPlace();
+        addPlace p = new addPlace();
         p.setAccuracy(50);
         p.setAddress("29, side layout, cohen 09");
         p.setLanguage("English-Ca");
         p.setPhone_number("4371233233");
         p.setWebsite("https://rahulshettyacaemy.com");
         p.setName("my house");
-        List<String> myList=new ArrayList<String>();
-        myList.addAll(Arrays.asList("shoepark","shoe"));
+        List<String> myList = new ArrayList<String>();
+        myList.addAll(Arrays.asList("shoepark", "shoe"));
         p.setTypes(myList);
-        location l=new location();
+        location l = new location();
         l.setLat(-38.383494);
         l.setLng(33.427362);
         p.setLocation(l);
-        RequestSpecification req =new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
-                .addQueryParam("key","qaclick123")
+        RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+                .addQueryParam("key", "qaclick123")
                 .setContentType(ContentType.JSON).build();
-       // given - request attached, when- where we are sending, then - assertion
-        ResponseSpecification resspec= new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-        RequestSpecification res= given().spec(req).body(p);
+        // given - request attached, when- where we are sending, then - assertion
+        RequestSpecification testReq = given().spec(req).body(p);
 
-         Response response=res.when().post("/maps/api/place/add/json")
+        ResponseSpecification resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+        Response response = testReq.when().post("/maps/api/place/add/json")
                 .then().spec(resspec).extract().response();
-        String responseAsString=response.asString();
+        String responseAsString = response.asString();
         System.out.println(responseAsString);
     }
 }
